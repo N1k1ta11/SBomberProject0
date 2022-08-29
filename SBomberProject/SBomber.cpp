b2764b16c58b79bd553b0953befdf00558f97939
NikitaKerzhakov <nikitakerzhik@gmail.com>
2022-08-29 17:19:10 +0500

    реализовал паттерн iterator

--- SBomberProject/SBomber.cpp
+++ SBomberProject/SBomber.cpp
@@ -226,14 +226,27 @@ vector<Bomb*> SBomber::FindAllBombs() const
 {
     vector<Bomb*> vecBombs;
 
-    for (size_t i = 0; i < vecDynamicObj.size(); i++)
+    /*for (size_t i = 0; i < vecDynamicObj.size(); i++)
     {
         Bomb* pBomb = dynamic_cast<Bomb*>(vecDynamicObj[i]);
         if (pBomb != nullptr)
         {
             vecBombs.push_back(pBomb);
         }
+    }*/
+    Iter arr(vecDynamicObj);
+
+    Iter::FindBombIterator it=arr.begin();
+    for (; it != arr.end(); ++it)
+    {
+        vecBombs.push_back((Bomb*)(*it));
     }
+
+    /*for (auto i : vecDynamicObj)
+    {
+        vecBombs.push_back(i);
+    }*/
+
 
     return vecBombs;
 }
--- SBomberProject/SBomber.h
+++ SBomberProject/SBomber.h
@@ -8,6 +8,8 @@
 #include "Ground.h"
 #include "Tank.h"
 
+using namespace std;
+
 class SBomber
 {
 public:
@@ -50,4 +52,103 @@ private:
     uint64_t startTime, finishTime, passedTime;
     uint16_t bombsNumber, deltaTime, fps;
     int16_t score;
+};
+
+
+class Iter
+{
+private:
+	vector<DynamicObject*>bombs;
+public:
+	Iter(vector<DynamicObject*>v)
+	{
+		for (auto i : v)
+		{
+			bombs.push_back(i);
+		}
+	}
+
+	void push_back(DynamicObject* n)
+	{
+		bombs.push_back(n);
+	}
+	class FindBombIterator
+	{
+	private:
+		vector<DynamicObject*>& refBombs;
+		int index;
+		DynamicObject* ptr;
+	public:
+		FindBombIterator(vector<DynamicObject*>& b) :refBombs(b), index(-1), ptr(nullptr) { ++(*this); }
+		void reset() { index = -1; ptr = nullptr; }
+		FindBombIterator& operator++()
+		{
+			index++;
+			if (index == -1)
+			{
+				index = 0;
+			}
+			for (; index < refBombs.size(); index++)
+			{
+				Bomb* pBomb = dynamic_cast<Bomb*>(refBombs[index]);
+				if (pBomb!=nullptr)
+				{
+					ptr = refBombs[index];
+					break;
+				}
+			}
+			if (index == refBombs.size())
+			{
+				index = -1;
+				ptr = nullptr;
+			}
+			return *this;
+		}
+
+		FindBombIterator& operator--()
+		{
+			if (index == -1)
+			{
+				index = refBombs.size() - 1;
+			}
+			for (; index >= 0; index--)
+			{
+				Bomb* pBomb = dynamic_cast<Bomb*>(refBombs[index]);
+				if (pBomb != nullptr)
+				{
+					ptr = refBombs[index];
+					break;
+				}
+			}
+			if (index == -1)
+			{
+				ptr = nullptr;
+			}
+			return *this;
+		}
+
+		DynamicObject* operator*() // операция разыменования итератора
+		{
+			return refBombs.at(index);
+		}
+
+		bool operator==(FindBombIterator it) // проверка на лог. равенство итераторов
+		{
+			if (index == it.index &&
+				ptr == it.ptr &&
+				refBombs == it.refBombs)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		bool operator!=(FindBombIterator it) // проверка на лог. неравенство
+		{
+			return !(*this == it);
+		}
+	};
+
+	FindBombIterator begin() { FindBombIterator it(bombs); return it; }
+	FindBombIterator end() { FindBombIterator it(bombs); it.reset(); return it; }
 };
