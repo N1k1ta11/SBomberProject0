8e0670f193147d61155f2ac7f75c3ab5cbd4d7bc
NikitaKerzhakov <nikitakerzhik@gmail.com>
2022-09-02 19:52:04 +0500

    Revert "частично реализовал паттерн Builder, осталось понять как изменять функцию Draw, чтобы появлялся другой дом"
    This reverts commit 241ac604e6c04010373d63dfb9877b9314b121e2.

--- SBomberProject/House.h
+++ SBomberProject/House.h
@@ -15,35 +15,7 @@ public:
 	void Draw() const override;
 
 private:
+
 	const uint16_t score = 40;
 };
 
-class HouseBuilder
-{
-protected:
-	House* h;
-public:
-	virtual void createHouse() {}
-	virtual House* getHouse() { return h; }
-};
-
-class YellowHouse : public HouseBuilder
-{
-public:
-	void createHouse() { h = new House; }
-};
-
-class BlueHouse : public HouseBuilder
-{
-	void createHouse() { h = new House; }
-};
-
-class Director
-{
-public:
-	House* createHouse(HouseBuilder& builder)
-	{
-		builder.createHouse();
-		return builder.getHouse();
-	}
-};
